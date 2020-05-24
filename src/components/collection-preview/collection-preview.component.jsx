b30de642b3c0b82e1b2a831ce0de7ed14ca25626
Wambura <brianwambura96@gmail.com>
2020-05-24 13:02:47 +0300

    Updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-import CollectionItem from '../collection-item/collection-item.component'
+import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.scss';
 
-const CollectionPreview = ({ title, items }) => (
-	<div className="collection-preview">
-		<h1 className="title">{title.toUpperCase()}</h1>
-		<div className="preview">
-			{items
-				.filter((item, index) => index < 4)
-				.map(({id, ...otherItemProps}) => (
-					<CollectionItem key={id} {...otherItemProps}/>
-				))}
+const CollectionPreview = ({ title, items }) => {
+	return (
+		<div className="collection-preview">
+			<h1 className="title">{title.toUpperCase()}</h1>
+			<div className="preview">
+				{items
+					.filter((item, index) => index < 4)
+					.map((item) => (
+						<CollectionItem key={item.id} item={item} />
+					))}
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default CollectionPreview;
